b24e03992c2b0cb601dac4d64e0d52afe3b75c71
happycoper <kevin.wirth@t-online.de>
2026-04-07 08:25:42 +0200

    new login content

--- VL2/routes.py
+++ VL2/routes.py
@@ -1,24 +1,33 @@
 import sqlite3
 from . import app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, make_response
 from pathlib import Path
 
 APP_DIR = Path(__file__).resolve().parent
 DB_PATH = APP_DIR /"v2.db"
 
-def get_db_connection():
+def get_db_connection(): # Verbindung zur Datenbank herstellen
     conn = sqlite3.connect(DB_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
-def init_db():
+def init_db(): #erstellung der Tabelle, wenn sie noch nicht existiert
     conn =get_db_connection()
     cur = conn.cursor()
+
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS users ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "username TEXT UNIQUE NOT NULL, "
+        "password TEXT NOT NULL)"
+    )
+
     cur.execute(
         "CREATE TABLE IF NOT EXISTS items ("
         "id INTEGER PRIMARY KEY AUTOINCREMENT, "
         "title TEXT NOT NULL, "
-        "content TEXT NOT NULL, "
+        "content TEXT NOT NULL CHECK(LENGTH(content) >= 1024), "
+        "priority Integer DEFAULT 2, " 
         "created_by TEXT NOT NULL)"
     )
     conn.commit()
@@ -33,11 +42,25 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-   if request.method == 'POST':
-       theuser = request.form.get('username')
-       thepass = request.form.get('password')
-       print(f"{theuser}, {thepass}")
-   return render_template('login.html')
+    message = None
+    if request.method == 'POST':
+        theuser = request.form.get('username')
+        thepass = request.form.get('password')
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users WHERE username = ?", (theuser,))
+        user = cur.fetchone()
+        conn.close()
+
+        if user and user["password"] == thepass:
+            response = make_response(redirect(url_for('tickets')))
+            response.set_cookie('username', theuser)
+            return response
+        else:
+            message = "Login fehlgeschlagen"
+
+    return render_template('login.html', message=message)
 
 @app.route('/tickets')
 def tickets():
@@ -58,4 +81,3 @@ def register():
 
        print(f"\n Username: {theuser},\n Email: {email},\n Passwort1: {thepass1},\n Passwort2: {thepass2} \n")
    return render_template('register.html')
-
