b969e6c80238aa8c6bbaff91ed1c79ac65a58997
happycoper <kevin.wirth@t-online.de>
2026-06-02 15:29:40 +0200

    commits von asus server

--- VL2/app.py
+++ VL2/app.py
@@ -2,7 +2,7 @@
 from VL2 import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0',port=5050, debug=True)
     
 
 # Starten vom Projekt-Root aus mit:
